8efe19af32744f8ddb3b2c924c6423914050bd2e
TriPhan-hcm <hoangtri.phan1995@outlook.com>
2020-06-21 00:01:05 +0700

    Spring boot security Login Ver 1

---
+++ src/main/java/com/o7planning/SpringBootLogin1Application.java
@@ -0,0 +1,13 @@
+package com.o7planning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLogin1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLogin1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/o7planning/SpringBootLogin1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.o7planning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLogin1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
